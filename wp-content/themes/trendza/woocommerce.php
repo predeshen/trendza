2ea0d12da8cb3ce5eb370d00b17212dc53125c37
Predeshen Naidoo <predeshen@gmail.com>
2026-09-03 13:57:29 +0200

    feat: improve WooCommerce storefront shell

--- wp-content/themes/trendza/woocommerce.php
+++ wp-content/themes/trendza/woocommerce.php
@@ -1,1 +1,9 @@
-<?php get_header(); ?><main id="main-content" class="container section"><div class="woocommerce"><?php woocommerce_content(); ?></div></main><?php get_footer(); ?>
+<?php
+get_header();
+?>
+<main id="main-content" class="container section">
+    <div class="woocommerce trendza-commerce-shell">
+        <?php woocommerce_content(); ?>
+    </div>
+</main>
+<?php get_footer(); ?>
