65a08e477143dba07e533bde5d0a4bf145ed798d
Predeshen Naidoo <predeshen@gmail.com>
2026-09-21 13:47:28 +0200

    Test supplier metadata safeguards

--- wp-content/plugins/trendza-core/tests/WooCommerceProductImporterTest.php
+++ wp-content/plugins/trendza-core/tests/WooCommerceProductImporterTest.php
@@ -6,4 +6,13 @@ final class WooCommerceProductImporterTest extends TestCase {
     public function testSupplierAttributeMetaKeyIsDefined(): void {
         self::assertSame('_trendza_supplier_attributes', ProductMeta::SUPPLIER_ATTRIBUTES);
     }
+
+    public function testSupplierCategoryMetaKeyIsDefined(): void {
+        self::assertSame('_trendza_supplier_categories', ProductMeta::SUPPLIER_CATEGORIES);
+    }
+
+    public function testSupplierManagedMetadataKeysRemainDistinct(): void {
+        self::assertNotSame(ProductMeta::SUPPLIER_ATTRIBUTES, ProductMeta::SUPPLIER_CATEGORIES);
+        self::assertNotSame(ProductMeta::SUPPLIER_COST, ProductMeta::SUPPLIER_RRP);
+    }
 }
